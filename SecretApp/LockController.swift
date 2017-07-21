9aea2fae31fd7aef55e158e2121756796611ba7a
TPPFIT iCloud <KienTruong@TPPFITs-Mac-mini.local>
2017-07-21 11:42:53 +0700

    Animation App

---
+++ SecretApp/LockController.swift
@@ -0,0 +1,35 @@
+//
+//  LockController.swift
+//  SecretApp
+//
+//  Created by TPPFIT iCloud on 6/22/17.
+//  Copyright © 2017 TPPFIT iCloud. All rights reserved.
+//
+
+import UIKit
+
+class LockController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
